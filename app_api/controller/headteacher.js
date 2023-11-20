85776820af6fc814da2b350ddcd946cc41013db9
Mzala123 <justicemwanzamj@gmail.com>
2023-11-20 08:53:39 +0200

    table teacher and learner

--- app_api/controller/headteacher.js
+++ app_api/controller/headteacher.js
@@ -11,120 +11,120 @@ const sendJSONResponse = (res, status, content)=>{
 
 
 module.exports.learners_list_by_institution_id = (req, res)=>{
-    const ObjectId = mongoose.Types.ObjectId
+     const ObjectId = mongoose.Types.ObjectId
      var institutionId = req.params.institutionId
-    console.log("hie there learner! " +institutionId)
+     console.log("hie there learner! " +institutionId)
 
-    Institution
-      .find({}, {institution_id:institutionId})
-      .exec()
-      .then((data)=>{
-        sendJSONResponse(res, 200, data)
-      })
-
-
-    // Institution
-    //   .aggregate(
-    //     [
-    //         {
-    //             $match:{institution_id: institutionId}
-    //         },
-    //         {
-    //             $project:{
-    //                 username:1,
-    //                 usertype_name:1,
-    //                 personId:1,
-    //                 institution_name:1,
-    //                 institution_address:1,
-    //                 nationalId:1,
-    //                 firstname:1,
-    //                 lastname:1,
-    //                 gender:1,
-    //                 place_residence:1,
-    //                 institutionId:1
-    //             }
-    //         },
-    //         {
-    //           $lookup:{
-    //             from:'people',
-    //             localField:'institution_id',
-    //             foreignField:'institutionId',
-    //             as: 'learnerDocs'
-    //           }
-    //         },
-    //         {
-    //             $unwind: '$learnerDocs'
-    //         },
-    //         {
-    //             $lookup:{
-    //                 from: 'users',
-    //                 localField: 'learnerDocs._id',
-    //                 foreignField:'personId',
-    //                 as:'userDetails'
-    //             }
-    //         }
-    //     ]
-    //   ).exec()
-    //   .then((data)=>{
-    //     sendJSONResponse(res, 200, data)
-    //   }).catch((error)=>{
-    //       sendJSONResponse(res, 401, error)
-    //   })
+            Institution.aggregate([
+                {
+                $match: {institution_id: {$eq: +institutionId}}
+                },
+                {
+                $project: {
+                    institution_id: 1,
+                    institution_name: 1,
+                    username:1,
+                    nationalId:1,
+                    firstname:1,
+                    lastname:1,
+                    gender:1,
+                    dob:1
+                }
+                },
+                {
+                    $lookup:{
+                        from:'people',
+                        localField:'institution_id',
+                        foreignField:'institutionId',
+                        as:'learnerDocs'
+                    }
+                },
+                {
+                    $unwind:'$learnerDocs'
+                },
+                {
+                    $lookup:{
+                        from:'users',
+                        localField:'learnerDocs._id',
+                        foreignField:'personId',
+                        as:'userDetails'
+                    }
+                },
+                {
+                    $unwind:'$userDetails'
+                }
+                ,
+                {
+                    $match:{'userDetails.usertype_name':'Learner'}
+                }
+            ]).exec()
+                .then((data) => {
+                sendJSONResponse(res, 200, data);
+                })
+                .catch((error) => {
+                console.error(error);
+                sendJSONResponse(res, 401, error);
+                });
 
 
 }
 
 module.exports.teachers_list_by_institution_id = (req, res)=>{
-    const ObjectId = mongoose.Types.ObjectId
-    var institutionId = req.params.institutionId
 
-    console.log("hie there teacher! " +institutionId)
-    //return institutionId;
-    Institution
-      .aggregate(
-        [
-            {
-                $match:{institution_id: {$eq: institutionId}}
-            },
-            {
-                $project:{
+    const ObjectId = mongoose.Types.ObjectId
+     var institutionId = req.params.institutionId
+     console.log("hie there learner! " +institutionId)
+
+            Institution.aggregate([
+                {
+                $match: {institution_id: {$eq: +institutionId}}
+                },
+                {
+                $project: {
+                    institution_id: 1,
+                    institution_name: 1,
                     username:1,
-                    usertype_name:1,
-                    personId:1,
-                    institution_name:1,
-                    institution_address:1,
                     nationalId:1,
                     firstname:1,
                     lastname:1,
                     gender:1,
-                    place_residence:1,
-                    institutionId:1
+                    dob:1
                 }
-            },
-            {
-              $lookup:{
-                from:'people',
-                localField:'institution_id',
-                foreignField:'institutionId',
-                as: 'learnerDocs'
-              }
-            },
-            {
-                $unwind: '$learnerDocs'
-            },
-            {
-                $lookup:{
-                    from: 'users',
-                    localField: 'learnerDocs._id',
-                    foreignField:'personId',
-                    as:'userDetails'
+                },
+                {
+                    $lookup:{
+                        from:'people',
+                        localField:'institution_id',
+                        foreignField:'institutionId',
+                        as:'learnerDocs'
+                    }
+                },
+                {
+                    $unwind:'$learnerDocs'
+                },
+                {
+                    $lookup:{
+                        from:'users',
+                        localField:'learnerDocs._id',
+                        foreignField:'personId',
+                        as:'userDetails'
+                    }
+                },
+                {
+                    $unwind:'$userDetails'
                 }
-            }
-        ]
-      ).exec()
-      .then((data)=>{
-        sendJSONResponse(res, 200, data)
-      }).catch((error)=>{
-          sendJSONResponse(res, 401, error)
-      })
+                ,
+                {
+                    $match:{'userDetails.usertype_name':'Teacher'}
+                }
+            ]).exec()
+                .then((data) => {
+                sendJSONResponse(res, 200, data);
+                })
+                .catch((error) => {
+                console.error(error);
+                sendJSONResponse(res, 401, error);
+                });
+
+
 }
